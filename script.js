7643bc3d7d52949ddf1a8fd834942b078aeb7949
princy2507 <143063519+princy2507@users.noreply.github.com>
2023-10-03 20:02:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,287 @@
+var quizData = {
+  TIME$WORK:  [
+    {
+      question: '1. A can do a work in 15 days and B in 20 days. If they work on it together for 4 days, then the fraction of the work that is left is :?',
+      options: ['1/4','1/10','7/15','8/15'],
+      answer: '8/15',
+    },
+    {
+      question: '2. A can lay railway track between two given stations in 16 days and B can do the same job in 12 days. With help of C, they did the job in 4 days only. Then, C alone can do the job in:?',
+      options: ['9*1/5', '9*2/5', '9*3/5', '10'],
+      answer: '9*3/5',
+    },
+    {
+      question: '3. A, B and C can do a piece of work in 20, 30 and 60 days respectively. In how many days can A do the work if he is assisted by B and C on every third day?',
+      options: ['12 days','15 days','16 days','18 days'],
+      answer: '15 days',
+    },
+    {
+      question: '5. A alone can do a piece of work in 6 days and B alone in 8 days. A and B undertook to do it for Rs. 3200. With the help of C, they completed the work in 3 days. How much is to be paid to C?',
+      options: ['375','400','600','800'],
+      answer: '400',
+    },
+    {
+      question: '6. If 6 men and 8 boys can do a piece of work in 10 days while 26 men and 48 boys can do the same in 2 days, the time taken by 15 men and 20 boys in doing the same type of work will be:',
+      options: ['4 days','5 days','III only','I or II or III'],
+      answer: 'I or II or III',
+    },
+    {
+      question: '7.A can do a piece of work in 4 hours; B and C together can do it in 3 hours, while A and C together can do it in 2 hours. How long will B alone take to do it?',
+      options: ['8 hours','10 hours','12 hours','24 hours'],
+      answer: '24 hours',
+    },
+    {
+      question: '8. A can do a certain work in the same time in which B and C together can do it. If A and B together could do it in 10 days and C alone in 50 days, then B alone could do it in:?',
+      options: ['15 days', '20 days', '25 days', '30 days'],
+      answer: '25 days',
+    },
+    {
+      question: '9. A does 80% of a work in 20 days. He then calls in B and they together finish the remaining work in 3 days. How long B alone would take to do the whole work?',
+      options: ['23 days','37 days','37*1/2','40 days'],
+      answer: '37*1/2',
+    },
+    {
+      question: '10. A machine P can print one lakh books in 8 hours, machine Q can print the same number of books in 10 hours while machine R can print them in 12 hours. All the machines are started at 9 A.M. while machine P is closed at 11 A.M. and the remaining two machines complete work. Approximately at what time will the work (to print one lakh books) be finished ?',
+      options: ['11.30 AM', '12 noon', '12.30 PM', '1.00 PM'],
+      answer: '1.00 PM',
+    },
+  ],
+  PROFIT$LOSS:[
+    {
+      question:'1. Alfred buys an old scooter for Rs. 4700 and spends Rs. 800 on its repairs. If he sells the scooter for Rs. 5800, his gain percent is:',
+      options: ['4*4/7%','5*5/11%','10%','12%'],
+      answer: '5*5/11%',
+    },
+   {
+      question:'2. The cost price of 20 articles is the same as the selling price of x articles. If the profit is 25%, then the value of x is:',
+      options: ['15','16','18','25'],
+      answer: '16',
+    },
+    {
+      question:'3. If selling price is doubled, the profit triples. Find the profit percent.',
+      options: ['100','66*2/3','105*1/3','120'],
+      answer: '100',
+    },
+   {
+      question:'4. In a certain store, the profit is 320% of the cost. If the cost increases by 25% but the selling price remains constant, approximately what percentage of the selling price is the profit?',
+      options: ['30%','70%','100%','250%'],
+      answer: '70%',
+    },
+    {
+      question:'5. A vendor bought toffees at 6 for a rupee. How many for a rupee must he sell to gain 20%?',
+      options: ['3','4','5','6'],
+      answer: '5',
+    },
+    {
+      question:'6. The percentage profit earned by selling an article for Rs. 1920 is equal to the percentage loss incurred by selling the same article for Rs. 1280. At what price should the article be sold to make 25% profit?',
+      options: ['2000','2200','2400','data in adequate'],
+      answer: '2000',
+    },
+    {
+      question:'7. A shopkeeper expects a gain of 22.5% on his cost price. If in a week, his sale was of Rs. 392, what was his profit?',
+      options: ['18.20','70','72','88.25'],
+      answer: '72',
+    },
+    {
+      question:'8. A man buys a cycle for Rs. 1400 and sells it at a loss of 15%. What is the selling price of the cycle?',
+      options: ['1090','1160','1190','1202'],
+      answer: '1190',
+    },
+    {
+      question:'9. Sam purchased 20 dozens of toys at the rate of Rs. 375 per dozen. He sold each one of them at the rate of Rs. 33. What was his percentage profit?',
+      options: ['3.5','4.5','5.6','6.5'],
+      answer: '5.6',
+    },
+    {
+      question:'10. Some articles were bought at 6 articles for Rs. 5 and sold at 5 articles for Rs. 6. Gain percent is:',
+      options: ['30%','33*1/3','35%','44%'],
+      answer: '44%',
+    },
+  ],
+  SI: [
+    {
+      question:'1. A sum of money at simple interest amounts to Rs. 815 in 3 years and to Rs. 854 in 4 years. The sum is:',
+      options: ['Rs. 650','Rs. 690','Rs. 698','Rs. 700'],
+      answer: 'Rs. 698',
+    },
+    {
+      question:'2. Mr. Thomas invested an amount of Rs. 13,900 divided in two different schemes A and B at the simple interest rate of 14% p.a. and 11% p.a. respectively. If the total amount of simple interest earned in 2 years be Rs. 3508, what was the amount invested in Scheme B?',
+      options: ['Rs. 6400','Rs. 6500','Rs. 7200','Rs. 7500','None of these'],
+      answer: 'Rs. 6400',
+    },
+   {
+      question:'3. A sum fetched a total simple interest of Rs. 4016.25 at the rate of 9 p.c.p.a. in 5 years. What is the sum?',
+      options: ['Rs. 4462.50','Rs. 8032.50','Rs. 8900','Rs. 8925'],
+      answer: 'Rs. 8925',
+   },
+   {
+    question:'4. How much time will it take for an amount of Rs. 450 to yield Rs. 81 as interest at 4.5% per annum of simple interest?',
+    options: ['3.5 years','4 years','4.5 years','5 years'],
+    answer: '4 years',
+   },
+   {
+     question:'5. Reena took a loan of Rs. 1200 with simple interest for as many years as the rate of interest. If she paid Rs. 432 as  interest at the end of the loan period, what was the rate of interest?',
+     options: ['3.6','6','18','Cannot be determined','None of these'],
+     answer: '6',
+   },
+   {
+    question:'6. A sum of Rs. 12,500 amounts to Rs. 15,500 in 4 years at the rate of simple interest. What is the rate of interest?',
+    options: ['3%','4%','5%','6%'],
+    answer: '6%',
+   },
+   {
+    question:'7. An automobile financier claims to be lending money at simple interest, but he includes the interest every six months for calculating the principal. If he is charging an interest of 10%, the effective rate of interest becomes:',
+    options: ['10%','10.25%','10.5%','None of these'],
+    answer: '10.25%',
+   },
+   {
+    question:'8. A lent Rs. 5000 to B for 2 years and Rs. 3000 to C for 4 years on simple interest at the same rate of interest and received Rs. 2200 in all from both of them as interest. The rate of interest per annum is:',
+    options:['5%','71%','8','10%'],
+    answer: '10%',
+   },
+   {
+    question:'9. A sum of Rs. 725 is lent in the beginning of a year at a certain rate of interest. After 8 months, a sum of Rs. 362.50 more is lent but at the rate twice the former. At the end of the year, Rs. 33.50 is earned as interest from both the loans. What was the original rate of interest?',
+    options: ['3.6%','4.5%','6%','None of these'],
+    answer: 'None of these',
+   },
+   {
+    question:'10. A man took loan from a bank at the rate of 12% p.a. simple interest. After 3 years he had to pay Rs. 5400 interest only for the period. The principal amount borrowed by him was:',
+    options: ['Rs. 2000','Rs. 10,000','Rs. 15,000'],
+    answer: 'Rs. 15,000',
+   },
+    
+  ],
+};
+
+var currentQuiz = [];
+var currentQuestionIndex = 0;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+
+function startQuiz(category) {
+  currentQuiz = quizData[category];
+  currentQuestionIndex = 0;
+  correctAnswers = 0;
+  wrongAnswers = 0;
+  
+  showQuestion();
+  
+  document.getElementById("quiz").style.display = "block";
+  document.getElementById("result").style.display = "none";
+}
+function showQuestion() {
+  var questionElement = document.getElementById("question");
+  var optionsElement = document.getElementById("options");
+  
+  var currentQuestion = currentQuiz[currentQuestionIndex];
+  
+  questionElement.innerHTML = currentQuestion.question;
+  
+  optionsElement.innerHTML = "";
+  for (var i = 0; i < currentQuestion.options.length; i++) {
+    var option = document.createElement("div");
+    option.classList.add("option");
+    
+    var radioBtn = document.createElement("input");
+    radioBtn.type = "radio";
+    radioBtn.name = "answer";
+    radioBtn.value = i + 1;
+    option.appendChild(radioBtn);
+    
+    var label = document.createElement("label");
+    label.innerHTML = currentQuestion.options[i];
+    option.appendChild(label);
+    
+    optionsElement.appendChild(option);
+  }
+}
+
+function checkAnswer() {
+  var selectedAnswer = document.querySelector('input[name="answer"]:checked');
+  
+  if (selectedAnswer) {
+    var answer = parseInt(selectedAnswer.value);
+    var currentQuestion = currentQuiz[currentQuestionIndex];
+    
+    var options = document.getElementsByClassName("option");
+    for (var i = 0; i < options.length; i++) {
+      options[i].classList.remove("correct-answer");
+      options[i].classList.remove("wrong-answer");
+    }
+    
+    if (answer === currentQuestion.answer) {
+      selectedAnswer.parentNode.classList.add("correct-answer");
+      correctAnswers++;
+    } else {
+      selectedAnswer.parentNode.classList.add("wrong-answer");
+      showCorrectAnswer();
+      wrongAnswers++;
+    }
+    
+    document.getElementById("correct").innerHTML = correctAnswers;
+    document.getElementById("wrong").innerHTML = wrongAnswers;
+    
+    disableOptions();
+  }
+}
+
+function showCorrectAnswer() {
+  var currentQuestion = currentQuiz[currentQuestionIndex];
+  var options = document.getElementsByClassName("option");
+  
+  for (var i = 0; i < options.length; i++) {
+    if (parseInt(options[i].querySelector("input").value) === currentQuestion.answer) {
+      options[i].classList.add("correct-answer");
+    }
+  }
+}
+
+function disableOptions() {
+  var options = document.getElementsByTagName("input");
+  
+  for (var i = 0; i < options.length; i++) {
+    options[i].disabled = true;
+  }
+  
+  document.getElementById("submit").disabled = true;
+}
+
+function nextQuestion() {
+  var selectedAnswer = document.querySelector('input[name="answer"]:checked');
+  
+  if (selectedAnswer) {
+    selectedAnswer.parentNode.classList.remove("correct-answer");
+    selectedAnswer.parentNode.classList.remove("wrong-answer");
+    selectedAnswer.checked = false;
+    selectedAnswer.disabled = false;
+  }
+  
+  document.getElementById("submit").disabled = false;
+  
+  currentQuestionIndex++;
+  
+  if (currentQuestionIndex < currentQuiz.length) {
+    showQuestion();
+  } else {
+    showResult();
+  }
+}
+function previousQuestion() {
+  currentQuestionIndex--;
+  if (currentQuestionIndex >= 0) {
+    showQuestion();
+  } 
+}
+
+function showResult() {
+  document.getElementById("quiz").style.display = "none";
+  document.getElementById("result").style.display = "block";
+  
+  document.getElementById("correct").innerHTML = correctAnswers;
+  document.getElementById("wrong").innerHTML = wrongAnswers;
+}
+
+function restartQuiz() {
+  document.getElementById("quiz").style.display = "none";
+  document.getElementById("result").style.display = "none";
+}
